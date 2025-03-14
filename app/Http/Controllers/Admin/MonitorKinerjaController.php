aebbe366f27ee68113cab59f375f1e9d36587f15
Dikhsantibong1432 <dwiranggatibongdikhsan@gmail.com>
2025-03-14 15:11:24 +0800

    Update MonitorKinerjaController and index.blade.php

--- app/Http/Controllers/Admin/MonitorKinerjaController.php
+++ app/Http/Controllers/Admin/MonitorKinerjaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\DailySummary;
+use App\Models\PowerPlant;
 use Illuminate\Http\Request;
 
 class MonitorKinerjaController extends Controller
@@ -12,6 +13,9 @@ class MonitorKinerjaController extends Controller
     {
         // Get the latest daily summary
         $latestSummary = DailySummary::latest()->first();
+
+        // Get power plant locations
+        $powerPlants = PowerPlant::select('name', 'latitude', 'longitude')->get();
 
         // Get data for the chart (last 7 days)
         $chartData = DailySummary::select('eaf', 'sof', 'efor', 'sdof', 'ncf', 'created_at')
@@ -58,6 +62,7 @@ class MonitorKinerjaController extends Controller
                 'nphr' => $latestSummary->nphr ?? 0,
                 'slc' => $latestSummary->slc ?? 0,
             ],
+            'powerPlants' => $powerPlants,
             'chartData' => $chartData,
         ];
 

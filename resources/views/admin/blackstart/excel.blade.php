595ac596439debfa76b244e6553c2a944fa1760c
ardykaaw <151797651+ardykaaw@users.noreply.github.com>
2025-05-12 16:36:55 +0800

    excel blackstarts

--- resources/views/admin/blackstart/excel.blade.php
+++ resources/views/admin/blackstart/excel.blade.php
@@ -1,27 +1,26 @@
 <table>
-    <!-- Header -->
-    <tr class="main-header">
-        <td colspan="10">Data Blackstart - {{ $blackstarts->first()->tanggal->format('F Y') }}</td>
-    </tr>
-    
-    <!-- Logo spacing row -->
+    <!-- Row for logo (leave empty, logo will be injected by WithDrawings) -->
+    <tr><td colspan="27"></td></tr>
+    <!-- Title -->
     <tr>
-        <td colspan="4"></td>
-        <td></td>
-        <td colspan="5"></td>
+        <td colspan="27" style="text-align: center; font-size: 16px; font-weight: bold; padding: 10px 0;">
+            Data Blackstart - {{ $blackstarts->first()->tanggal->format('F Y') }}
+        </td>
     </tr>
-    <tr><td colspan="10"></td></tr>
+    <tr><td colspan="27"></td></tr>
 
     <!-- Blackstart Data Section -->
     <tr class="section-header">
-        <td colspan="10">Komitmen dan Pembahasan</td>
+        <td colspan="12">Komitmen dan Pembahasan</td>
     </tr>
     <tr class="table-header">
         <th>No</th>
         <th>Unit Layanan / Sentral</th>
         <th>Pembangkit</th>
         <th>Black Start</th>
+        <th>Evidence Diagram</th>
         <th>SOP Black Start</th>
+        <th>Evidence SOP</th>
         <th>Load Set</th>
         <th>Line Energize</th>
         <th>Status Jaringan</th>
@@ -34,7 +33,25 @@
         <td>{{ $blackstart->powerPlant->name }}</td>
         <td>{{ ucfirst($blackstart->pembangkit_status) }}</td>
         <td>{{ ucfirst($blackstart->black_start_status) }}</td>
+        <td>
+            @if($blackstart->diagram_evidence)
+                <a href="{{ asset('storage/' . $blackstart->diagram_evidence) }}" target="_blank">
+                    {{ basename($blackstart->diagram_evidence) }}
+                </a>
+            @else
+                -
+            @endif
+        </td>
         <td>{{ ucfirst($blackstart->sop_status) }}</td>
+        <td>
+            @if($blackstart->sop_evidence)
+                <a href="{{ asset('storage/' . $blackstart->sop_evidence) }}" target="_blank">
+                    {{ basename($blackstart->sop_evidence) }}
+                </a>
+            @else
+                -
+            @endif
+        </td>
         <td>{{ ucfirst($blackstart->load_set_status) }}</td>
         <td>{{ ucfirst($blackstart->line_energize_status) }}</td>
         <td>{{ ucfirst($blackstart->status_jaringan) }}</td>
@@ -42,7 +59,7 @@
         <td>{{ strtoupper($blackstart->status) }}</td>
     </tr>
     @endforeach
-    <tr><td colspan="10"></td></tr>
+    <tr><td colspan="12"></td></tr>
 
     <!-- Peralatan Blackstart Section -->
     <tr class="section-header">
@@ -107,29 +124,77 @@
             <!-- Kompresor Diesel -->
             <td>{{ $peralatan->kompresor_diesel_jumlah }}</td>
             <td>{{ ucfirst($peralatan->kompresor_diesel_kondisi) }}</td>
-            <td>{{ $peralatan->kompresor_eviden ? basename($peralatan->kompresor_eviden) : '-' }}</td>
+            <td>
+                @if($peralatan->kompresor_eviden)
+                    <a href="{{ asset('storage/' . $peralatan->kompresor_eviden) }}" target="_blank">
+                        {{ basename($peralatan->kompresor_eviden) }}
+                    </a>
+                @else
+                    -
+                @endif
+            </td>
             <!-- Tabung Udara -->
             <td>{{ $peralatan->tabung_udara_jumlah }}</td>
             <td>{{ ucfirst($peralatan->tabung_udara_kondisi) }}</td>
-            <td>{{ $peralatan->tabung_eviden ? basename($peralatan->tabung_eviden) : '-' }}</td>
+            <td>
+                @if($peralatan->tabung_eviden)
+                    <a href="{{ asset('storage/' . $peralatan->tabung_eviden) }}" target="_blank">
+                        {{ basename($peralatan->tabung_eviden) }}
+                    </a>
+                @else
+                    -
+                @endif
+            </td>
             <!-- UPS -->
             <td>{{ ucfirst($peralatan->ups_kondisi) }}</td>
             <!-- Lampu Emergency -->
             <td>{{ $peralatan->lampu_emergency_jumlah }}</td>
             <td>{{ ucfirst($peralatan->lampu_emergency_kondisi) }}</td>
-            <td>{{ $peralatan->lampu_eviden ? basename($peralatan->lampu_eviden) : '-' }}</td>
+            <td>
+                @if($peralatan->lampu_eviden)
+                    <a href="{{ asset('storage/' . $peralatan->lampu_eviden) }}" target="_blank">
+                        {{ basename($peralatan->lampu_eviden) }}
+                    </a>
+                @else
+                    -
+                @endif
+            </td>
             <!-- Battery Catudaya -->
             <td>{{ $peralatan->battery_catudaya_jumlah }}</td>
             <td>{{ ucfirst($peralatan->battery_catudaya_kondisi) }}</td>
-            <td>{{ $peralatan->catudaya_eviden ? basename($peralatan->catudaya_eviden) : '-' }}</td>
+            <td>
+                @if($peralatan->catudaya_eviden)
+                    <a href="{{ asset('storage/' . $peralatan->catudaya_eviden) }}" target="_blank">
+                        {{ basename($peralatan->catudaya_eviden) }}
+                    </a>
+                @else
+                    -
+                @endif
+            </td>
             <!-- Battery Black Start -->
             <td>{{ $peralatan->battery_blackstart_jumlah }}</td>
             <td>{{ ucfirst($peralatan->battery_blackstart_kondisi) }}</td>
-            <td>{{ $peralatan->blackstart_eviden ? basename($peralatan->blackstart_eviden) : '-' }}</td>
+            <td>
+                @if($peralatan->blackstart_eviden)
+                    <a href="{{ asset('storage/' . $peralatan->blackstart_eviden) }}" target="_blank">
+                        {{ basename($peralatan->blackstart_eviden) }}
+                    </a>
+                @else
+                    -
+                @endif
+            </td>
             <!-- Radio Komunikasi -->
             <td>{{ $peralatan->radio_jumlah }}</td>
             <td>{{ ucfirst($peralatan->radio_komunikasi_kondisi) }}</td>
-            <td>{{ $peralatan->radio_eviden ? basename($peralatan->radio_eviden) : '-' }}</td>
+            <td>
+                @if($peralatan->radio_eviden)
+                    <a href="{{ asset('storage/' . $peralatan->radio_eviden) }}" target="_blank">
+                        {{ basename($peralatan->radio_eviden) }}
+                    </a>
+                @else
+                    -
+                @endif
+            </td>
             <!-- Simulasi Black Start -->
             <td>{{ ucfirst($peralatan->simulasi_blackstart) }}</td>
             <!-- Start Kondisi Black Out -->

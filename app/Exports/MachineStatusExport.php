1c11fea6d8140dded25b9383c8d717c10483b525
Dikhsantibong1432 <dwiranggatibongdikhsan@gmail.com>
2025-05-14 05:11:09 +0800

    tambah download periode

--- app/Exports/MachineStatusExport.php
+++ app/Exports/MachineStatusExport.php
@@ -6,21 +6,24 @@ use Maatwebsite\Excel\Concerns\FromView;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithColumnWidths;
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
 
-class MachineStatusExport implements FromView, ShouldAutoSize, WithColumnWidths
+class MachineStatusExport implements FromView, ShouldAutoSize, WithColumnWidths, WithMultipleSheets
 {
     protected $powerPlants;
     protected $logs;
     protected $hops;
-    protected $date;
+    protected $dates;
     protected $selectedInputTime;
 
-    public function __construct($powerPlants, $logs, $hops, $date, $selectedInputTime)
+    public function __construct($powerPlants, $logs, $hops, $dates, $selectedInputTime)
     {
         $this->powerPlants = $powerPlants;
         $this->logs = $logs;
         $this->hops = $hops;
-        $this->date = $date;
+        $this->dates = is_array($dates) ? $dates : [$dates, $dates];
         $this->selectedInputTime = $selectedInputTime;
     }
 
@@ -38,13 +41,38 @@ class MachineStatusExport implements FromView, ShouldAutoSize, WithColumnWidths
         ];
     }
 
+    public function sheets(): array
+    {
+        $sheets = [];
+        
+        // Get all dates between start and end date
+        $period = CarbonPeriod::create($this->dates[0], $this->dates[1]);
+        
+        foreach ($period as $date) {
+            $dateStr = $date->format('Y-m-d');
+            $sheets[] = new MachineStatusSheet(
+                $this->powerPlants,
+                $this->logs->filter(function($log) use ($dateStr) {
+                    return $log->tanggal->format('Y-m-d') === $dateStr;
+                }),
+                $this->hops->filter(function($hop) use ($dateStr) {
+                    return $hop->tanggal->format('Y-m-d') === $dateStr;
+                }),
+                $dateStr,
+                $this->selectedInputTime
+            );
+        }
+        
+        return $sheets;
+    }
+
     public function view(): View
     {
         return view('admin.machine-status.export-excel', [
             'powerPlants' => $this->powerPlants,
             'logs' => $this->logs,
             'hops' => $this->hops,
-            'date' => $this->date,
+            'date' => $this->dates[0]->format('Y-m-d') . ' to ' . $this->dates[1]->format('Y-m-d'),
             'selectedInputTime' => $this->selectedInputTime,
             'navlog_path' => public_path('logo/navlog1.png'),
             'k3_path' => public_path('logo/k3_logo.png')
--- app/Http/Controllers/Admin/MachineStatusController.php
+++ app/Http/Controllers/Admin/MachineStatusController.php
@@ -297,7 +297,8 @@ class MachineStatusController extends Controller
     public function exportExcel(Request $request)
     {
         try {
-            $date = $request->get('date', now()->toDateString());
+            $startDate = $request->get('start_date', now()->toDateString());
+            $endDate = $request->get('end_date', $startDate);
             $selectedInputTime = $request->get('input_time');
             
             // Query untuk mengambil data pembangkit
@@ -314,15 +315,15 @@ class MachineStatusController extends Controller
             
             $powerPlants = $query->get();
             
-            // Ambil semua log untuk tanggal dan waktu input yang dipilih
-            $logs = MachineStatusLog::whereDate('tanggal', $date)
+            // Ambil semua log untuk rentang tanggal dan waktu input yang dipilih
+            $logs = MachineStatusLog::whereBetween('tanggal', [$startDate, $endDate])
                 ->when($selectedInputTime, function($query) use ($selectedInputTime) {
                     return $query->where('input_time', $selectedInputTime);
                 })
                 ->get();
 
-            // Ambil data HOP
-            $hops = UnitOperationHour::whereDate('tanggal', $date)
+            // Ambil data HOP untuk rentang tanggal
+            $hops = UnitOperationHour::whereBetween('tanggal', [$startDate, $endDate])
                 ->when(session('unit') !== 'mysql', function($query) {
                     $query->whereHas('powerPlant', function($q) {
                         $q->where('unit_source', session('unit'));
@@ -335,10 +336,14 @@ class MachineStatusController extends Controller
                 })
                 ->get();
 
-            $fileName = 'laporan-status-mesin-' . $date . '.xlsx';
+            $fileName = 'laporan-status-mesin-' . $startDate;
+            if ($startDate !== $endDate) {
+                $fileName .= '-sampai-' . $endDate;
+            }
+            $fileName .= '.xlsx';
             
             return Excel::download(
-                new MachineStatusExport($powerPlants, $logs, $hops, $date, $selectedInputTime),
+                new MachineStatusExport($powerPlants, $logs, $hops, [$startDate, $endDate], $selectedInputTime),
                 $fileName
             );
 

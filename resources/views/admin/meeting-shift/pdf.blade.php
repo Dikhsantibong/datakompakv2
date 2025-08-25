a57538e6fa5d2854cf4e3d6d93773b51a7b6d983
Dikhsantibong <tibongdikhsan@gmail.com>
2025-08-26 00:01:07 +0500

    perbaikan

--- resources/views/admin/meeting-shift/pdf.blade.php
+++ resources/views/admin/meeting-shift/pdf.blade.php
@@ -69,8 +69,41 @@
     </style>
 </head>
 <body>
+    @php
+        $unitLogoMap = [
+            'PLTD POASIA' => 'logo/PLTD_POASIA.png',
+            'PLTD KOLAKA' => 'logo/PLTD_KOLAKA.png',
+            'PLTD BAU BAU' => 'logo/PLTD_BAU_BAU.png',
+            'PLTD WUA WUA' => 'logo/PLTD_WUA_WUA.png',
+            'PLTD WINNING' => 'logo/PLTM_WINNING.png',
+            'PLTD ERKEE' => 'logo/PLTD_EREKE.png',
+            'PLTD LADUMPI' => 'logo/PLTD_LADUMPI.png',
+            'PLTD LANGARA' => 'logo/PLTD_LANGARA.png',
+            'PLTD LANIPA-NIPA' => 'logo/PLTD_LANIPA_NIPA.png',
+            'PLTD PASARWAJO' => 'logo/PLTD_PASARWAJO.png',
+            'PLTD POASIA CONTAINERIZED' => 'logo/PLTD_POASIA_CONTAINERIZED.png',
+            'PLTD RAHA' => 'logo/PLTD_RAHA.png',
+            'PLTD WAJO' => 'logo/PLTD_WAJO.png',
+            'PLTD WANGI-WANGI' => 'logo/PLTD_WANGI_WANGI.png',
+            'PLTM RONGI' => 'logo/PLTM_RONGI.png',
+            'PLTM SABILAMBO' => 'logo/PLTM_SABILAMBO.png',
+            'PLTD KENDARI' => 'logo/PLTMG_KENDARI.png',
+            'PLTD BARUTA' => 'logo/PLTU_BARUTA.png',
+            'PLTD MORAMO' => 'logo/PLTU_MORAMO.png',
+            'PLTM MIKUASI' => 'logo/PLTM_MIKUASI.png',
+        ];
+        $unitName = $meetingShift->unit_name ?? '';
+        $logoPath = 'logo/UP_KENDARI.png';
+        foreach ($unitLogoMap as $key => $path) {
+            if (stripos($unitName, $key) !== false) {
+                $logoPath = $path;
+                break;
+            }
+        }
+    @endphp
     <div class="header-container">
         <img src="{{ public_path('logo/navlog1.png') }}" alt="PLN Nusantara Power Logo" class="logo">
+        <img src="{{ public_path($logoPath) }}" alt="Unit Logo" style="position: absolute; top: 0; right: 0; height: 60px;">
     </div>
 
     <div class="header">
